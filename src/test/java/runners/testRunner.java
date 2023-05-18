2dd412c08dc48a9d6ed7c680c35b8b9b6e00c2a8
ibrahimebrarakcakoyun <ibrahim@akcakoyun.com.tr>
2023-05-18 20:59:05 +0300

    v0.1

---
+++ src/test/java/runners/testRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+
+)
+
+
+
+
+
+
+
+public class testRunner {
+
+
+}
